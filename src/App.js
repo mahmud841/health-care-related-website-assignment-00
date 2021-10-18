e8e7839dee1d45a0fdc3cf4eb798b48099a959f7
AL MAHMUD SURUJ <mahmudsuruj14@gmail.com>
2021-10-19 04:24:42 +0800

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
+import { BrowserRouter as Router, Switch,Route } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Doctors from './components/Doctors/Doctors';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import NotFound from './components/NotFound/NotFound';
+import Services from './components/Services/Services';
+import Footer from './components/Shared/Footer/Footer';
+import Header from './components/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-   <h1>Health Care Diagonostic Centre </h1>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="/services">
+            <Services></Services>
+          </Route>
+          <Route path="/doctors">
+            <Doctors></Doctors>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+      </Router>
+
+
+
     </div>
   );
 }
---
+++ src/components/Shared/Footer/Footer.js
@@ -0,0 +1,67 @@
+import { faFacebookF, faGooglePlusG, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import './Footer.css';
+
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div>
+        <div>
+          <div>
+            <p>Our Services</p>
+          </div>
+          <div>
+            <small>Emergency Patients Care</small>
+            <br />
+            <small>Sergery Department</small>
+            <br />
+            <small>Treatment Personal Diseases</small>
+            <br />
+            <small>Medicine Department </small>
+            <br />
+            <small>Cardiology Department</small>
+          </div>
+        </div>
+        <div>
+          <div>
+            <p> Dental Health</p>
+          </div>
+          <div>
+            <small>Emergency Dental Care</small>
+            <br />
+            <small>Check Up</small>
+            <br />
+            <small>Treatment of Personal Diseases</small>
+            <br />
+            <small>Tooth Extraction</small>
+            <br />
+            <small>Check Up</small>
+          </div>
+        </div>
+        <div>
+          <div>
+            <p>Our Address</p>
+          </div>
+          <div>
+            <small>Dhaka,Bangladesh</small>
+            <div className="brandIcon">
+              <FontAwesomeIcon icon={faFacebookF} />
+              
+              <FontAwesomeIcon icon={faGooglePlusG} />
+              <FontAwesomeIcon icon={faTwitter} />
+            </div>
+            <small>Call Now</small>
+            <p className="callNow">+8801338164898</p>
+          </div>
+        </div>
+      </div>
+      <div style={{ textAlign: "center", marginTop: "30px" }}>
+        <small>Copyright 2020 All Right Reserved</small>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
